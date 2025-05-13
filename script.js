dabc731d00d56812e50dd64af4e6d8ed19303a5f
AlAqilBS <samudra88.alaqilbintang@gmail.com>
2025-05-13 15:38:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const menuIcon = document.getElementById("menu-icon");
   menuIcon.addEventListener("click", () => {
     navMenu.classList.toggle("active");
   });
+
+document.addEventListener("click", function (e) {
+  if (!menuIcon.contains(e.target) && !navMenu.contains(e.target)) {
+    navMenu.classList.remove("active");
+  }
+});
